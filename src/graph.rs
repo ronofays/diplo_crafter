d95faba4317ec7971476a3be9a49303328f9a8f0
Ronofays <ronofays@gmail.com>
2024-09-25 15:14:43 -0500

    turkey test

--- src/graph.rs
+++ src/graph.rs
@@ -8,7 +8,7 @@ enum SupplyType {
 
 enum LandType {
     Normal,
-    SupplyCenter,
+    SupplyCenter(SupplyType),
 }
 
 enum TerritoryType {
@@ -17,13 +17,15 @@ enum TerritoryType {
 }
 
 struct Node {
+    name: String,
     attributes: TerritoryType,
     neighbors: Vec<NodeWeakRef>,
 }
 
 impl Node {
-    fn new(attributes: TerritoryType) -> Self {
+    fn new(name: String, attributes: TerritoryType) -> Self {
         Node{
+            name,
             attributes,
             neighbors: Vec::new(),
         }
@@ -45,8 +47,8 @@ impl Graph {
 
 impl Graph {
     // Add a new node to the graph and return a strong reference to it
-    fn add_node(&mut self, attributes: TerritoryType) -> NodeRef {
-        let node = Rc::new(RefCell::new(Node::new(attributes)));
+    fn add_node(&mut self, node: Node) -> NodeRef {
+        let node = Rc::new(RefCell::new(node));
         self.nodes.push(node.clone());
         node
     }
@@ -84,5 +86,164 @@ impl Graph {
                 !Rc::ptr_eq(&neighbor_strong, node1)
             })
         });
+    }
+}
+
+fn gen_test_turkey() -> Graph {
+    let mut g = Graph::new();
+
+    let con = Node::new(
+        "Constantinople".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let ank = Node::new(
+        "Ankara".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let smy = Node::new(
+        "Smyrna".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let con = g.add_node(con);
+    let smy = g.add_node(smy);
+    let ank = g.add_node(ank);
+
+    g.add_edge(&con, &ank);
+    g.add_edge(&con, &smy);
+    g.add_edge(&ank, &smy);
+
+    g
+}
+
+fn gen_test_turkey_region() -> Graph {
+    let mut g = Graph::new();
+
+    let con = Node::new(
+        "Constantinople".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let ank = Node::new(
+        "Ankara".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let smy = Node::new(
+        "Smyrna".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Turkey".to_string()))
+        )
+    );
+
+    let sev = Node::new(
+        "Sevastopol".to_string(),
+        TerritoryType::Land(
+            LandType::SupplyCenter(
+                SupplyType::Core("Russia".to_string()))
+        )
+    );
+
+    let bla = Node::new(
+        "Black Sea".to_string(),
+        TerritoryType::Sea
+    );
+
+    let eas = Node::new(
+        "Eastern Mediterranean".to_string(),
+        TerritoryType::Sea
+    );
+
+    let arm = Node::new(
+        "Armenia".to_string(),
+        TerritoryType::Land(LandType::Normal)
+    );
+
+    let syr = Node::new(
+        "Syria".to_string(),
+        TerritoryType::Land(LandType::Normal)
+    );
+
+    let con = g.add_node(con);
+    let smy = g.add_node(smy);
+    let ank = g.add_node(ank);
+    let sev = g.add_node(sev);
+    let bla = g.add_node(bla);
+    let eas = g.add_node(eas);
+    let arm = g.add_node(arm);
+    let syr = g.add_node(syr);
+
+    g.add_edge(&con, &ank);
+    g.add_edge(&con, &smy);
+    g.add_edge(&con, &bla);
+
+    g.add_edge(&ank, &bla);
+    g.add_edge(&ank, &smy);
+    g.add_edge(&ank, &arm);
+
+    g.add_edge(&smy, &eas);
+    g.add_edge(&smy, &arm);
+    g.add_edge(&smy, &syr);
+
+    g.add_edge(&sev, &bla);
+    g.add_edge(&sev, &arm);
+
+    g.add_edge(&eas, &syr);
+    g.add_edge(&bla, &arm);
+    g.add_edge(&arm, &syr);
+
+    g
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_turkey_nodes() {
+        let turkey = gen_test_turkey();
+
+        // Build a map from node names to NodeRefs for easy access
+        let mut node_map: HashMap<String, NodeRef> = HashMap::new();
+        for node_ref in &turkey.nodes {
+            let node = node_ref.borrow();
+            node_map.insert(node.name.clone(), Rc::clone(node_ref));
+        }
+
+        // Check that all expected nodes are present
+        let expected_nodes = vec![
+            "Constantinople",
+            "Ankara",
+            "Smyrna",
+        ];
+        assert_eq!(node_map.len(), expected_nodes.len());
+
+        for name in &expected_nodes {
+            assert!(
+                node_map.contains_key(*name),
+                "Node '{}' is missing from the graph",
+                name
+            );
+        }
     }
 }
